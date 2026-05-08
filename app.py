ecc8841855459fe7d582d82e1145a863efc41eb0
wandymarweinn-dotcom <wandymarweinn@gmail.com>
2026-05-08 23:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,6 @@ def download():
     return send_file(buf, mimetype='image/png', as_attachment=True, download_name='qr_code.png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
